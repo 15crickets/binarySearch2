ba69ec5255c67241616612cd30e9e3adc0257602
sc30714 <vikram.vasudevan@comcast.net>
2024-03-24 16:01:50 -0700

    Update

---
+++ Node.h
@@ -0,0 +1,31 @@
+#ifndef NODE_H
+#define NODE_H
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+
+class Node{
+ public:
+  Node();
+  ~Node();
+  //initializing variables
+  Node* parent;
+  Node* left;
+  Node* right;
+  int data;
+  //function prototypes
+  void setRight(Node* newRight);
+  void setLeft(Node* newLeft);
+  Node* getRight();
+  Node* getLeft();
+  void setParent(Node* newparent);
+  Node* getParent();
+  void setInformation(int value);
+  int getInformation();
+ 
+};
+
+
+#endif
